64b53f1422a3b13f5474c778877ff7e645e4e410
DESTROYER3999 <49817693+DESTROYER3999@users.noreply.github.com>
2021-06-08 15:54:51 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+console.log("Hi there")
+console.log("DM me #DESTROYER")
+const submitBtn = document.getElementById("submit-btn");
+
+submitBtn.onclick = (e) => {
+	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
